4b7e4c9988eaa891879ff023ecdacc9c95c3d040
Oluyedi Oluwasegun Stephen <oluyedis@gmail.com>
2022-10-30 14:56:22 +0100

    added profile image

--- src/components/Profileheader.js
+++ src/components/Profileheader.js
@@ -1,17 +1,11 @@
 import React from "react";
-// import avatarShareIcon from "../assets/_Avatar share button.png";
 import { BsThreeDots } from "react-icons/bs";
 import { RiShareForwardLine } from "react-icons/ri";
-// import avatarShareIconMobile from "../assets/_Avatar share button-mobile.png";
-import profileImg from "../assets/profile__img.png";
+import profileImg from "../assets/Profile-pic.jpg.jpeg";
 
 export default function Profileheader() {
   return (
     <div className="profile-container">
-      {/* <button className="profile-share-btn">
-        <img className="profile-big-share-btn" src={avatarShareIcon} alt="" />
-        <img className="profile-small-share-btn" src={BsThreeDots} alt="" />
-      </button> */}
       <div className="profile-share-btn">
         <div className="profile-big-share-btn">
           <RiShareForwardLine />
@@ -20,10 +14,9 @@ export default function Profileheader() {
           <BsThreeDots />
         </div>
       </div>
-      {/* <div>{BsThreeDots}</div> */}
 
       <img id="profile__img" src={profileImg} alt="" />
-      <h5 id="profile-name"> Annette Black</h5>
+      <h5 id="profile-name">Oluwasegun Oluyedi</h5>
     </div>
   );
 }
